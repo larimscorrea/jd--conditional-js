c8d91cc09e1ca2bf12d017d99dc12347b886586c
Larissa Corrêa <101229830+larimscorrea@users.noreply.github.com>
2022-09-13 00:00:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+/*Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão)*/
+/*var n = prompt('Digite um número ');
+var resultado = n % 2 === 0 ? 'par' : 'impar';
+document.write(`O número ${n} é ${resultado}`);*/
+
+/*Faça um Programa que peça a hora e apresente uma mensagem de "Bom dia", "Boa tarde" ou "Boa noite" conforme a hora informada.*/
+/*var hour = 1 * prompt('Digite a hora em formato 24 horas');
+var hourNow = hour.getHours();
+switch (hourNow) {
+  case 0 >= 5:
+    console.log('Boa madrugada');
+    break;
+  case 5 >= 11:
+    console.log('Bom dia');
+    break;
+  case 12 >= 18:
+    console.log('Bom tarde');
+    break;
+  case 18 >= 24:
+    console.log('Boa noite');
+    break;
+}
+
+/*Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.*/
+/*var agora = new Date();
+var diaSem = agora.getDay();
+switch (diaSem) {
+  case 1:
+    console.log('Domingo');
+    break;
+
+  case 2:
+    console.log('Segunda');
+    break;
+  case 3:
+    console.log('Terça');
+    break;
+  case 4:
+    console.log('Quarta');
+    break;
+  case 5:
+    console.log('Quinta');
+    break;
+  case 6:
+    console.log('Sexta');
+    break;
+  default:
+    console.log('Sábado');
+}
+As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes. 
+Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+   * salários até R$ 280,00 (incluindo) : aumento de 20%
+   * salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+   * salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+
+   - o salário antes do reajuste;
+   - o percentual de aumento aplicado;
+   - o valor do aumento;
+   - o novo salário, após o aumento. */
